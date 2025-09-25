dd331ed150d7aa9625edd94041c17d29b0bb6e71
whitegonist <cheuoni@gmail.com>
2025-09-25 14:51:03 +0900

    a

---
+++ java1/src/foo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class foo {
+    public static void main(String[] args){
+       int a = 33,b=13;
+        int big = (a>b) ? a : b;
+        System.out.println("big = " +big);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+                                                         
